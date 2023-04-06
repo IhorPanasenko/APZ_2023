4f1c03b385e3b8efd98470367769fef441affe2c
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-06 17:39:47 +0300

    implement interface

--- InsuranceDiscountsWeb/DAL/Repositories/UserRepository.cs
+++ InsuranceDiscountsWeb/DAL/Repositories/UserRepository.cs
@@ -1,4 +1,8 @@
-﻿using DAL.Interfaces;
+﻿using Core.Models;
+using DAL.Interfaces;
+using InsuranceDiscountsWeb.Managers;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +11,54 @@ using System.Threading.Tasks;
 
 namespace DAL.Services
 {
-    public class UserRepository: IUserREpository
+    public class UserRepository: IUserRepository
     {
+        private UserManager<IdentityUser> userManager;
+
+        public UserRepository(UserManager<IdentityUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public async Task<UserManagerResponse> RegisterUserAsync(RegisterModel registerModel)
+        {
+            if(registerModel == null)
+            {
+                throw new ArgumentNullException("RegisterModel is null");
+            }
+
+            if(registerModel.Password != registerModel.ConfirmPassword)
+            {
+                return new UserManagerResponse
+                {
+                    Message = "Confirm apassword doesn't match the password",
+                    IsSuccess = false
+                };
+            }
+
+            var identityUser = new IdentityUser
+            {
+                Email = registerModel.Email,
+                UserName = registerModel.Email
+            };
+
+            var result = await userManager.CreateAsync(identityUser, registerModel.Password);
+
+            if (result.Succeeded)
+            {
+                return new UserManagerResponse
+                {
+                    Message = "User created Succefully",
+                    IsSuccess = true
+                };
+            }
+
+            return new UserManagerResponse
+            {
+                Message = "User didn't created",
+                IsSuccess = false,
+                Errors = result.Errors.Select(e => e.Description)
+            };
+        }
     }
 }
