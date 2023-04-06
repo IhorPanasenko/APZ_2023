b3b77ae2d223ab7d045b3681348cf73b9f4978d6
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-06 17:40:01 +0300

    Create Auth controller

---
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using Core.Models;
+using DAL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace InsuranceDiscountsWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IUserRepository _userRepository;
+
+        public AuthController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterAsync([FromBody]RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Some model properties are not valid");
+            }
+
+            var result = await _userRepository.RegisterUserAsync(model);
+
+            if (!result.IsSuccess)
+            {
+                return BadRequest(result);
+            }
+
+            return Ok(result);  
+        }
+    }
+}
