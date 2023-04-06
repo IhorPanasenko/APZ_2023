e6f19de599de013fd8cf6edb6924dabc6cc81b27
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-06 17:39:30 +0300

    Add method to interface

--- InsuranceDiscountsWeb/DAL/Interfaces/IUserRepository.cs
+++ InsuranceDiscountsWeb/DAL/Interfaces/IUserRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Models;
+using InsuranceDiscountsWeb.Managers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace DAL.Interfaces
 {
-    public class IUserREpository
+    public interface IUserRepository
     {
+        Task<UserManagerResponse> RegisterUserAsync(RegisterModel registerModel);
     }
 }
