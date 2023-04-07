b1a9bf213fc5f8a8dbd7208a2eb8a06040ebe864
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-07 14:47:59 +0300

    Create Login Model

---
+++ InsuranceDiscountsWeb/Core/Models/LoginModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [EmailAddress]
+        [StringLength(50)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength =5)]
+        public string Password { get; set; }
+
+    }
+}
