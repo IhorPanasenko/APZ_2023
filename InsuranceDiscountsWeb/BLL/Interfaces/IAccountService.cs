ba212cee801fbd8bd39c3b5decf16e5d05112349
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-13 14:07:04 +0300

    Create IAccountService

---
+++ InsuranceDiscountsWeb/BLL/Interfaces/IAccountService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IAccountService
+    {
+        public Task<bool> Register(RegisterModel registerModel);
+        public Task<string> LogIn(LoginModel loginModel);
+        public Task LogOut();
+    }
+}
