156076d79f3c4d08f746369c40974063bfb71ed4
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-13 23:17:44 +0300

    Update RegisterViewModel.cs

--- InsuranceDiscountsWeb/InsuranceDiscountsWeb/ViewModels/RegisterViewModel.cs
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/ViewModels/RegisterViewModel.cs
@@ -15,10 +15,12 @@ namespace InsuranceDiscountsWeb.ViewModels
 
         [Required]
         [StringLength(50, MinimumLength = 5)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required]
         [StringLength(50, MinimumLength = 5)]
+        [DataType(DataType.Password)]
         public string ConfirmPassword { get; set; }
     }
 }
