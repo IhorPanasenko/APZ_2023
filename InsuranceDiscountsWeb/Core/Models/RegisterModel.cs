b8cd58408794ccf39a3c5332ae1fc929d5b3174f
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-13 14:06:53 +0300

    Add UserName

--- InsuranceDiscountsWeb/Core/Models/RegisterModel.cs
+++ InsuranceDiscountsWeb/Core/Models/RegisterModel.cs
@@ -15,6 +15,10 @@ namespace Core.Models
         public string Email { get; set; }
 
         [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
         [StringLength(50, MinimumLength = 5)]
         public string Password { get; set; }
 
