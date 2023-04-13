20551c86994ae4f0c4fffb0ada00591468578713
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-13 14:07:32 +0300

    Change AccountController

--- InsuranceDiscountsWeb/InsuranceDiscountsWeb/Controllers/AccountController.cs
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/Controllers/AccountController.cs
@@ -1,6 +1,9 @@
-﻿using Core.Models;
+﻿using BLL.Interfaces;
+using BLL.Services;
+using Core.Models;
 using InsuranceDiscountsWeb.Managers;
 using InsuranceDiscountsWeb.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -13,42 +16,82 @@ namespace InsuranceDiscountsWeb.Controllers
     {
         private readonly UserManager<IdentityUser> userManager;
         private readonly SignInManager<IdentityUser> signInManager;
+        private readonly ILogger<AccountController> logger;
+        private readonly IAccountService accountService;
 
-        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, ILogger<AccountController> logger, AccountService accountService)
         {
             this.userManager = userManager;
             this.signInManager = signInManager;
+            this.logger = logger;
+            this.accountService = accountService;
         }
 
         [HttpPost]
-        public async Task<IActionResult> Register (RegisterViewModel registerViewModel)
+        [AllowAnonymous]
+        [Route("Register")]
+        public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
         {
             if (!ModelState.IsValid)
             {
-                return BadRequest(ModelState);
+                return ValidationProblem(ModelState);
             }
 
-            if (registerViewModel.Password != registerViewModel.ConfirmPassword)
+            try
             {
-                return BadRequest("Confirm password doesn't match the password");
+                var registerModel = convertModel(registerViewModel);
+                var result = await accountService.Register(registerModel);
+
+                if (!result)
+                {
+                    return NotFound("For detailed information see logging");
+                }
+
+                return Ok("User Created successfully");
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return BadRequest(e.Message);
             }
 
-            var identityUser = new IdentityUser
-            {
-                Email = registerViewModel.Email,
-                UserName = registerViewModel.UserName
-            };
 
-            
-            var result = await userManager.CreateAsync(identityUser, registerViewModel.Password);
+        }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                return ValidationProblem(ModelState);
+            }
+
+            var result = await signInManager.PasswordSignInAsync(loginViewModel.EmailAddress, loginViewModel.Password, false, false);
 
             if (!result.Succeeded)
             {
-                return BadRequest("User didn't created");
+                return BadRequest(result);
             }
 
-            await signInManager.SignInAsync(identityUser, isPersistent: false);
-            return Ok("User Created successfully");
+            return Ok();
+        }
+
+        [HttpPost("LogOut")]
+        public async Task<IActionResult> LogOut()
+        {
+            await signInManager.SignOutAsync();
+            return Ok();
+        }
+
+        private RegisterModel convertModel(RegisterViewModel registerViewModel)
+        {
+            return new RegisterModel
+            {
+                Email = registerViewModel.Email,
+                UserName = registerViewModel.UserName,
+                Password = registerViewModel.Password,
+                ConfirmPassword = registerViewModel.ConfirmPassword,
+            };
         }
 
     }
