8a7f8831388e6db96f64979635626a578aaf5fe1
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-13 12:35:50 +0300

    Change RegisterViewModel

--- InsuranceDiscountsWeb/InsuranceDiscountsWeb/ViewModels/RegisterViewModel.cs
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/ViewModels/RegisterViewModel.cs
@@ -10,6 +10,10 @@ namespace InsuranceDiscountsWeb.ViewModels
         public string Email { get; set; }
 
         [Required]
+        [StringLength(50)]
+        public string UserName { get; set; } 
+
+        [Required]
         [StringLength(50, MinimumLength = 5)]
         public string Password { get; set; }
 
