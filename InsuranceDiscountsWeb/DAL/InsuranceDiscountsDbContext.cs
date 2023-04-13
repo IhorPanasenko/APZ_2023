94cc17c530231fa098c0f60145c9f6956d720b8c
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-13 12:18:31 +0300

    Addd OnModelCreating function

--- InsuranceDiscountsWeb/DAL/InsuranceDiscountsDbContext.cs
+++ InsuranceDiscountsWeb/DAL/InsuranceDiscountsDbContext.cs
@@ -10,13 +10,16 @@ using System.Threading.Tasks;
 
 namespace DAL
 {
-    public class InsuranceDiscountsDbContext : IdentityDbContext<IdentityUser>
+    public class InsuranceDiscountsDbContext : IdentityDbContext
     {
         public InsuranceDiscountsDbContext(DbContextOptions<InsuranceDiscountsDbContext> options) : base (options)
         {
-                
+            
         }
 
-        public DbSet<User> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);  
+        }
     }
 }
