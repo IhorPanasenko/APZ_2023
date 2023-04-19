b497c86319e08cc033eaf3df1afcdca5d5b3c8c5
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-19 15:11:27 +0300

    Add roles to account repository

--- InsuranceDiscountsWeb/DAL/Repositories/AccountRepository.cs
+++ InsuranceDiscountsWeb/DAL/Repositories/AccountRepository.cs
@@ -15,12 +15,19 @@ namespace DAL.Repositories
         private readonly UserManager<IdentityUser> userManager;
         private readonly SignInManager<IdentityUser> signInManager;
         private readonly ILogger<AccountRepository> logger;
+        private readonly RoleManager<IdentityRole> roleManager;
 
-        public AccountRepository(ILogger<AccountRepository> logger, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
+        public AccountRepository(
+            ILogger<AccountRepository> logger, 
+            SignInManager<IdentityUser> signInManager, 
+            UserManager<IdentityUser> userManager,
+            RoleManager<IdentityRole> roleManager
+            )
         {
             this.signInManager = signInManager;
             this.userManager = userManager;
             this.logger = logger;
+            this.roleManager = roleManager;
         }
 
         public async Task<string> LogIn(LoginModel loginModel)
@@ -46,12 +53,26 @@ namespace DAL.Repositories
                     throw new Exception(result.Errors.ToString());
                 }
 
+                await checkRole(registerModel.Role);
+
+                await userManager.AddToRoleAsync(identityUser, registerModel.Role);
+
                 return true;
             }
             catch (Exception e)
             {
                 logger.LogError(e.Message);
                 return false;
+            }
+        }
+
+        private async Task checkRole(string roleName)
+        {
+          var result = await roleManager.RoleExistsAsync(roleName);
+
+            if (!result)
+            {
+                await roleManager.CreateAsync(new IdentityRole { Name = roleName});
             }
         }
     }
