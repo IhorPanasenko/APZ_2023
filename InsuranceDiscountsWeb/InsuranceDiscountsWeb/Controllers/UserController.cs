f4b18cfb81d09f18a6364630fa68b842ec2e6e89
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-19 17:01:09 +0300

    Create UserController

---
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/Controllers/UserController.cs
@@ -0,0 +1,96 @@
+﻿using BLL.Interfaces;
+using Core.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace InsuranceDiscountsWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService userService;
+        private readonly ILogger<UserController> logger;
+
+        public UserController(IUserService userService, ILogger<UserController> logger)
+        {
+            this.userService = userService;
+            this.logger = logger;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAllUsers()
+        {
+            try
+            {
+                var users = await userService.GetAllUsers();
+                return Ok(users);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("GetByEmail")]
+        public async Task<IActionResult> GetByEmail(string email)
+        {
+            try
+            {
+                var user = await userService.GetUserByEmail(email);
+                return Ok(user);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("GetById")]
+        public async Task<IActionResult> GetById(string userId)
+        {
+            try
+            {
+                var user = await userService.GetUserByEmail(userId);
+                return Ok(user);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("Delete")]
+        public async Task<IActionResult> DeleteUser(string email)
+        {
+            try
+            {
+                return Ok(await userService.DeleteUser(email));
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e.Message);
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost("Update")]
+        public async Task<IActionResult> UpdateUser(string UserId, AppUser appUser)
+        {
+            try
+            {
+                return Ok(await userService.UpdateUser(UserId, appUser));
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e.Message);
+                return BadRequest(e.Message); 
+
+            }
+        }
+    }
+}
