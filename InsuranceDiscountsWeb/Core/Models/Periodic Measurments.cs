5726e02a060ec2a51f37dba4f4661f3b17629953
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-27 00:13:31 +0300

    Create periodic measurments model

---
+++ InsuranceDiscountsWeb/Core/Models/Periodic Measurments.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Periodic_Measurments
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public int Pulse { get; set; }
+
+        [Required]
+        public double Glucose { get; set; }
+
+        [Required]
+        public double Cholesterol { get; set; }
+
+        [Required]
+        public double BloodPreasure { get; set; }
+
+        [Required]
+        public DateTime MesurmentDate { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey("UserId")]
+        public Guid UserId { get; set; }
+
+        public AppUser? AppUser { get; set; }
+    }
+}
