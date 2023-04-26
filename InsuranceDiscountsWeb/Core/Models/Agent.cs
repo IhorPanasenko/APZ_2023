99fb763f051681df53127dc217287ad312838c79
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-26 23:50:57 +0300

    Add foreign Key annotation to agent model

--- InsuranceDiscountsWeb/Core/Models/Agent.cs
+++ InsuranceDiscountsWeb/Core/Models/Agent.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +31,7 @@ namespace Core.Models
         public double Raiting { get; set; }
 
         [Required]
+        [ForeignKey("CompanyId")]
         public Guid CompanyId { get; set; }
 
         [Required]
