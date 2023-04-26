546ea523d31ec77acd9e80802c8f856d31b7b5d2
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-27 00:06:57 +0300

    Create Static mesurments Model

---
+++ InsuranceDiscountsWeb/Core/Models/StaticMeasurments.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class StaticMeasurments
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public double Height { get; set; }
+
+        [Required]
+        public double Weight { get; set; }
+
+        [Required]
+        public double Waist { get; set; }
+    }
+}
