5218ffed5ee6921c691d517c190330e2cb9a7ea0
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-26 23:53:30 +0300

    Create Policy Class model

---
+++ InsuranceDiscountsWeb/Core/Models/Policy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Policy
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = String.Empty;
+
+        [Required]
+        public string Description { get; set; } = String.Empty;
+
+        [Required]
+        public double CoverageAmount { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [ForeignKey("CompanyId")]
+        public Guid CompanyId { get; set; }
+
+        public Company? Company { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Guid CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+        
+
+
+
+    }
+}
