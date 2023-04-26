78c9f769f8f7ff0863532be9931946cf31e2be44
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-26 23:46:32 +0300

    Create Agent class Model

---
+++ InsuranceDiscountsWeb/Core/Models/Agent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models
+{
+    public class Agent
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string FirstName { get; set; } = String.Empty;
+
+        [Required]
+        public string SecondName { get; set; } = String.Empty;
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; } = String.Empty;
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; } = String.Empty;
+
+        [Required]
+        public double Raiting { get; set; }
+
+        [Required]
+        public Guid CompanyId { get; set; }
+
+        [Required]
+        public Company? company { get; set; }
+    }
+}
