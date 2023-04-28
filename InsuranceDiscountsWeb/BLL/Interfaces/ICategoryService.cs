22e0002b14092cfc6dac8b47bf6c570eaa60013e
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-29 00:23:20 +0300

    Create category Service

---
+++ InsuranceDiscountsWeb/BLL/Interfaces/ICategoryService.cs
@@ -0,0 +1,22 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface ICategoryService
+    {
+        public Task<bool> Create(Category category);
+
+        public Task<bool> Update(Category category);
+
+        public Task<List<Category>> GetAll();
+
+        public Task<bool> Delete(Guid id);
+
+        public Task<Category?> GetById(Guid id);
+    }
+}
