5f0fbee1d77b47f78548cee14b4e100cc77713e1
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-30 12:24:19 +0300

    Create ICompanyService

---
+++ InsuranceDiscountsWeb/BLL/Interfaces/ICompanyService.cs
@@ -0,0 +1,23 @@
+﻿using Core.Models;
+using Core.Models.UpdateModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface ICompanyService
+    {
+        public Task<Company?> GetById(Guid id);
+
+        public Task<List<Company>> GetAll();
+
+        public Task<Company?> Create(Company company);
+
+        public Task<bool> Delete(Guid id);
+
+        public Task<bool> Update(UpdateCompanyModel company);
+    }
+}
