5b9fbbba8af49d37af2539fb98939d01e5a154a4
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-30 22:35:14 +0300

    Create AgentRepository.cs

---
+++ InsuranceDiscountsWeb/DAL/Repositories/AgentRepository.cs
@@ -0,0 +1,52 @@
+﻿using Core.Models;
+using Core.Models.UpdateModels;
+using DAL.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class AgentRepository:IAgentRepository
+    {
+        private readonly ILogger<AgentRepository> logger;
+        private readonly InsuranceDiscountsDbContext dbContext;
+
+        public AgentRepository(
+            ILogger<AgentRepository> logger,
+            InsuranceDiscountsDbContext dbContext
+            )
+        {
+            this.logger = logger;
+            this.dbContext = dbContext;
+        }
+
+        public Task<Agent> Create(Agent agent)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Agent>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Agent> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Agent> Update(UpdateAgentModel agent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
