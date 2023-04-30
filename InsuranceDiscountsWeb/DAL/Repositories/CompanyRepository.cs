9787f9c87af214e33d27e55237e95988d55da34c
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-30 12:05:08 +0300

    Start implementing ICompanyRepository

--- InsuranceDiscountsWeb/DAL/Repositories/CompanyRepository.cs
+++ InsuranceDiscountsWeb/DAL/Repositories/CompanyRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Core.Models;
+using DAL.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +9,52 @@ using System.Threading.Tasks;
 
 namespace DAL.Repositories
 {
-    public class CompanyRepository
+    public class CompanyRepository : ICompanyRepository
     {
+        private readonly InsuranceDiscountsDbContext dbContext;
+        private readonly ILogger<CompanyRepository> logger;
 
+        public CompanyRepository(
+            InsuranceDiscountsDbContext dbContext,
+            ILogger<CompanyRepository> logger
+            )
+        {
+            this.dbContext = dbContext;
+            this.logger = logger;
+        }
+        public async Task<Company?> Create(Company company)
+        {
+            try
+            {
+                await dbContext.Companies.AddAsync(company);
+                await dbContext.SaveChangesAsync();
+                return company;
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e.Message);
+                return null;
+            }
+        }
+
+        public Task<bool> Delete(Guid id)
+        {
+            
+        }
+
+        public Task<List<Company>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company?> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(Company company)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
