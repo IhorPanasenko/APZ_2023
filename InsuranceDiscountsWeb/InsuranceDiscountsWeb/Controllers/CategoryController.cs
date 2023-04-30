01f6b3f433d177a5dd2dea961c8a64e1ff8d0da5
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-30 11:27:19 +0300

    implement functions in CategoryController

--- InsuranceDiscountsWeb/InsuranceDiscountsWeb/Controllers/CategoryController.cs
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/Controllers/CategoryController.cs
@@ -1,4 +1,7 @@
 ﻿using BLL.Interfaces;
+using Core.Models;
+using Core.Models.UpdateModels;
+using InsuranceDiscountsWeb.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Runtime.CompilerServices;
@@ -15,7 +18,7 @@ namespace InsuranceDiscountsWeb.Controllers
         public CategoryController(
             ILogger<CategoryController> logger,
             ICategoryService categoryService
-            
+
             )
         {
             this.logger = logger;
@@ -23,26 +26,68 @@ namespace InsuranceDiscountsWeb.Controllers
         }
 
         [HttpPost("Create")]
-        public async Task<IActionResult> Create()
+        public async Task<IActionResult> Create(CategoryViewModel categoryViewModel)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest("Model is not valid");
+            }
 
+            try
+            {
+                var category = convert(categoryViewModel);
+                var res = await categoryService.Create(category);
+
+                if (!res)
+                {
+                    return BadRequest("Error's were found. Inspect logging");
+                }
+
+                return Ok(categoryViewModel);
+
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return BadRequest(e.Message);
+            }
         }
 
-        [HttpPost("Delete")]
 
+
+        [HttpDelete]
         public async Task<IActionResult> Delete(Guid id)
         {
+            try
+            {
+                var res = await categoryService.Delete(id);
 
+                if (!res)
+                {
+                    return NotFound("Errors happend. Watch logging");
+                }
+
+                return Ok($"category with id {id} was deleted");
+
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return BadRequest(e.Message);
+            }
         }
 
         [HttpGet("GetAll")]
         public async Task<IActionResult> GetAll()
         {
-            try{
+            try
+            {
                 var categories = await categoryService.GetAll();
-                return Ok(categories);
+                var viewModelsList = convert(categories); 
+                return Ok(viewModelsList);
             }
-            catch(Exception e){
+            catch (Exception e)
+            {
                 logger.LogError(e.Message);
                 return BadRequest(e.Message);
             }
@@ -54,16 +99,81 @@ namespace InsuranceDiscountsWeb.Controllers
             try
             {
                 var category = await categoryService.GetById(id);
-                return Ok(category);
+
+                if (category is null)
+                {
+                    return NotFound($"No category with {id} was found");
+                }
+
+                var categoryView = convert(category);
+                return Ok(categoryView);
             }
             catch (Exception e)
             {
                 logger.LogError(e.Message);
                 return BadRequest(e.Message);
             }
-
         }
 
+        [HttpPut("Update")]
+        public async Task<IActionResult> Update(UpdateCategoryViewModel categoryViewModel)
+        {
+            try
+            {
+                var category = convert(categoryViewModel);
+                var res = await categoryService.Update(category);
 
+                if (!res)
+                {
+                    return NotFound($"Cannot Update category with parameters\nid: {category.Id}\n name: {category.CategoryName}");
+                }
+
+                return Ok(res);
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return BadRequest(e.Message);
+            }
+        }
+
+        private IEnumerable<CategoryViewModel> convert(IEnumerable<Category> categories)
+        {
+            List<CategoryViewModel> viewModelsList = new List<CategoryViewModel>();
+
+            foreach (var category in categories)
+            {
+                viewModelsList.Add(convert(category));
+            }
+
+            return viewModelsList;
+        }
+
+        private Category convert(CategoryViewModel categoryViewModel)
+        {
+            return new Category
+            {
+                Id = categoryViewModel.Id,
+                CategoryName = categoryViewModel.CategoryName,
+            };
+        }
+
+        private CategoryViewModel convert(Category category)
+        {
+            return new CategoryViewModel
+            {
+                Id = category.Id,
+                CategoryName = category.CategoryName,
+            };
+        }
+
+        private UpdateCategoryModel convert (UpdateCategoryViewModel updateCategoryView)
+        {
+            return new UpdateCategoryModel
+            {
+                Id = updateCategoryView.Id,
+                CategoryName = updateCategoryView.CategoryName
+            };
+        }
     }
 }
