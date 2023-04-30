77c4d1f38977c001b9e03033c728faaacb5bdf04
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-30 12:25:03 +0300

    Create UpdateCompanyModel class

---
+++ InsuranceDiscountsWeb/Core/Models/UpdateModels/UpdateCompanyModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.UpdateModels
+{
+    public class UpdateCompanyModel
+    {
+        [Required]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string? CompanyName { get; set; }
+
+        public string? Description { get; set; }
+
+        [Required]
+        public string? Address { get; set; } 
+
+        [Phone]
+        public string? PhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string? EmailAddress { get; set; } 
+
+        [Url]
+        public string? WebsiteAddress { get; set; }
+    }
+}
