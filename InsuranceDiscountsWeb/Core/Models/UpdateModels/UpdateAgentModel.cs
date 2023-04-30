092a8563a1077372a2b97b85a91d0e6247b4b2f8
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-30 22:35:07 +0300

    Create UpdateAgentModel.cs

---
+++ InsuranceDiscountsWeb/Core/Models/UpdateModels/UpdateAgentModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.UpdateModels
+{
+    public class UpdateAgentModel
+    {
+        [Required]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string? FirstName { get; set; }
+
+        public string? SecondName { get; set; }
+
+        [Phone]
+        public string? PhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string? EmailAddress { get; set; }
+
+        public double Raiting { get; set; }
+
+        public Guid? CompanyId { get; set; }
+    }
+}
