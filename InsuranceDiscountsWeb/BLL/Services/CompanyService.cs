4fe2801297d2e68bffff74cbade4a14ca5ce7a7a
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-04-30 12:26:42 +0300

    start implementing interface for CompanyService

--- InsuranceDiscountsWeb/BLL/Services/CompanyService.cs
+++ InsuranceDiscountsWeb/BLL/Services/CompanyService.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BLL.Interfaces;
+using Core.Models;
+using Core.Models.UpdateModels;
+using DAL.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,42 @@ using System.Threading.Tasks;
 
 namespace BLL.Services
 {
-    public class CompanyService
+    public class CompanyService : ICompanyService
     {
+        private readonly ICompanyRepository companyRepository;
+        private readonly ILogger<CompanyService> logger;
+
+        public CompanyService(
+            ICompanyRepository companyRepository,
+            ILogger<CompanyService> logger
+            )
+        {
+            this.companyRepository = companyRepository;
+            this.logger = logger;
+        }
+        public Task<Company?> Create(Company company)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Company>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Company?> GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(UpdateCompanyModel company)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
