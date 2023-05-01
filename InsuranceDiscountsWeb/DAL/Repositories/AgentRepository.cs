eb98094d57d5a2ebff075e17b3c0ed213cb815d4
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-01 11:17:06 +0300

    Implement interafce

--- InsuranceDiscountsWeb/DAL/Repositories/AgentRepository.cs
+++ InsuranceDiscountsWeb/DAL/Repositories/AgentRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Models;
 using Core.Models.UpdateModels;
 using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
@@ -24,29 +25,116 @@ namespace DAL.Repositories
             this.dbContext = dbContext;
         }
 
-        public Task<Agent> Create(Agent agent)
+        public async Task<Agent?> Create(Agent agent)
         {
-            throw new NotImplementedException();
+            try
+            {
+                await dbContext.Agents.AddAsync(agent);
+                await dbContext.SaveChangesAsync();
+                return agent;
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e.Message);
+                return null;
+            }
+
         }
 
-        public Task<bool> Delete(Guid id)
+        public async Task<bool> Delete(Guid id)
         {
-            throw new NotImplementedException();
+            try
+            {
+               var agent = await dbContext.Agents.FindAsync(id);
+
+                if (agent is null)
+                {
+                    throw new Exception($"No agent with id {id} was found");
+                }
+
+                dbContext.Agents.Remove(agent);
+                await dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e.Message);
+                return false;
+            }
         }
 
-        public Task<List<Agent>> GetAll()
+        public async Task<List<Agent>> GetAll()
         {
-            throw new NotImplementedException();
+            List<Agent> agents = new List<Agent>();
+
+            try
+            {
+                 agents =await dbContext.Agents.ToListAsync();
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e.Message);
+            }
+
+            return agents;
         }
 
-        public Task<Agent> GetById(Guid id)
+        public async Task<Agent?> GetById(Guid id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var agent =await dbContext.Agents.FindAsync(id);
+
+                if(agent is null)
+                {
+                    throw new Exception("Can't create new Agents");
+                }
+
+                return agent;
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e.Message);
+                return null;
+            }
         }
 
-        public Task<Agent> Update(UpdateAgentModel agent)
+        public async Task<Agent?> Update(UpdateAgentModel agent)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var oldAgent = await dbContext.Agents.FindAsync(agent.Id);
+
+                if(oldAgent is null)
+                {
+                    throw new Exception($"Can't find agent for update with id {agent.Id}");
+                }
+
+                await update(oldAgent, agent);
+                dbContext.Agents.Update(oldAgent);
+                await dbContext.SaveChangesAsync();
+                return oldAgent;
+            }  
+            catch(Exception e)
+            {
+                logger.LogError(e.Message);
+                return null;
+            }
+        }
+
+        private async Task update(Agent agent ,UpdateAgentModel updateAgentModel)
+        {
+            agent.FirstName = String.IsNullOrEmpty(updateAgentModel.FirstName) ? agent.FirstName : updateAgentModel.FirstName;
+            agent.SecondName = String.IsNullOrEmpty(updateAgentModel.SecondName) ? agent.SecondName : updateAgentModel.SecondName;
+            agent.PhoneNumber = String.IsNullOrEmpty(updateAgentModel.PhoneNumber) ? agent.PhoneNumber : updateAgentModel.PhoneNumber;
+            agent.EmailAddress = String.IsNullOrEmpty(updateAgentModel.EmailAddress) ? agent.EmailAddress : updateAgentModel.EmailAddress;
+            agent.Raiting = updateAgentModel.Raiting < 0 ? agent.Raiting : updateAgentModel.Raiting;
+            
+            if(updateAgentModel.CompanyId != null)
+            {
+                agent.CompanyId = (Guid)updateAgentModel.CompanyId;
+                agent.Company = await dbContext.Companies.FindAsync(updateAgentModel.CompanyId);
+            }
         }
     }
 }
