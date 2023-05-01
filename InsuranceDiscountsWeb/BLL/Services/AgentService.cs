7864994e065a49120ffcf5cc4dbac5f8d4ceb9ab
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-01 11:50:59 +0300

    Implement AgentService interface

--- InsuranceDiscountsWeb/BLL/Services/AgentService.cs
+++ InsuranceDiscountsWeb/BLL/Services/AgentService.cs
@@ -15,39 +15,124 @@ namespace BLL.Services
     {
         private readonly ILogger<AgentService> logger;
         private readonly IAgentRepository agentRepository;
+        private readonly ICompanyRepository companyRepository;
 
         public AgentService(
             ILogger<AgentService> logger,
-            IAgentRepository agentRepository
+            IAgentRepository agentRepository,
+            ICompanyRepository companyRepository
             )
         {
             this.logger = logger;
             this.agentRepository = agentRepository;
+            this.companyRepository = companyRepository;
         }
 
-        public Task<Agent?> Create(Agent agent)
+        public async Task<bool> Create(Agent agent)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var resultAgent =await agentRepository.Create(agent);
+                
+                if(resultAgent is null)
+                {
+                    return false;
+                }
+
+                return true;
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e.Message);
+                return false;
+            }
         }
 
-        public Task<bool> Delete(Guid id)
+        public async Task<bool> Delete(Guid id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var result = await agentRepository.Delete(id);
+                return result;
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e.Message);
+                return false;
+            }
         }
 
-        public Task<List<Agent>> GetAll()
+        public async Task<List<Agent>> GetAll()
         {
-            throw new NotImplementedException();
+            List<Agent> agents = new List<Agent>();
+
+            try
+            {
+                agents = await agentRepository.GetAll();
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e.Message);   
+            }
+
+            return agents;
         }
 
-        public Task<Agent?> GetById(Guid id)
+        public async Task<Agent?> GetById(Guid id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var agent = await agentRepository.GetById(id);
+
+                if(agent == null)
+                {
+                    throw new Exception($"Can't find agent with id: {id}\nSee console logging for more information");
+                }
+
+                return agent;
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e.Message);
+                return null;
+            }
         }
 
-        public Task<Agent?> Update(UpdateAgentModel agent)
+        public async Task<Agent?> Update(UpdateAgentModel updateAgentModel)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var oldAgent = await agentRepository.GetById(updateAgentModel.Id);
+
+                if(oldAgent is null)
+                {
+                    throw new ArgumentException($"No agents with id {updateAgentModel.Id} was sfound for update");
+                }
+
+                await update(oldAgent, updateAgentModel);
+                var result = await agentRepository.Update(oldAgent);
+                return result;
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e.Message);
+                return null;
+            }
+        }
+
+        private async Task update(Agent agent, UpdateAgentModel updateAgentModel)
+        {
+            agent.FirstName = String.IsNullOrEmpty(updateAgentModel.FirstName) ? agent.FirstName : updateAgentModel.FirstName;
+            agent.SecondName = String.IsNullOrEmpty(updateAgentModel.SecondName) ? agent.SecondName : updateAgentModel.SecondName;
+            agent.PhoneNumber = String.IsNullOrEmpty(updateAgentModel.PhoneNumber) ? agent.PhoneNumber : updateAgentModel.PhoneNumber;
+            agent.EmailAddress = String.IsNullOrEmpty(updateAgentModel.EmailAddress) ? agent.EmailAddress : updateAgentModel.EmailAddress;
+            agent.Raiting = updateAgentModel.Raiting < 0 ? agent.Raiting : updateAgentModel.Raiting;
+
+            if (updateAgentModel.CompanyId != null)
+            {
+                agent.CompanyId = (Guid)updateAgentModel.CompanyId;
+                agent.Company = await companyRepository.GetById(updateAgentModel.Id!) ;
+            }
         }
     }
 }
