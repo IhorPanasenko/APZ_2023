cb30c2947882406d7359d563a67662f74c92dbe6
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-01 11:50:36 +0300

    Update AgentRepository.cs

--- InsuranceDiscountsWeb/DAL/Repositories/AgentRepository.cs
+++ InsuranceDiscountsWeb/DAL/Repositories/AgentRepository.cs
@@ -31,6 +31,9 @@ namespace DAL.Repositories
             {
                 await dbContext.Agents.AddAsync(agent);
                 await dbContext.SaveChangesAsync();
+
+                var company = await dbContext.Companies.FindAsync(agent.CompanyId);
+                agent.Company = company;
                 return agent;
             }
             catch(Exception e)
@@ -99,41 +102,18 @@ namespace DAL.Repositories
             }
         }
 
-        public async Task<Agent?> Update(UpdateAgentModel agent)
+        public async Task<Agent?> Update(Agent agent)
         {
             try
             {
-                var oldAgent = await dbContext.Agents.FindAsync(agent.Id);
-
-                if(oldAgent is null)
-                {
-                    throw new Exception($"Can't find agent for update with id {agent.Id}");
-                }
-
-                await update(oldAgent, agent);
-                dbContext.Agents.Update(oldAgent);
+                dbContext.Agents.Update(agent);
                 await dbContext.SaveChangesAsync();
-                return oldAgent;
+                return agent;
             }  
             catch(Exception e)
             {
                 logger.LogError(e.Message);
                 return null;
-            }
-        }
-
-        private async Task update(Agent agent ,UpdateAgentModel updateAgentModel)
-        {
-            agent.FirstName = String.IsNullOrEmpty(updateAgentModel.FirstName) ? agent.FirstName : updateAgentModel.FirstName;
-            agent.SecondName = String.IsNullOrEmpty(updateAgentModel.SecondName) ? agent.SecondName : updateAgentModel.SecondName;
-            agent.PhoneNumber = String.IsNullOrEmpty(updateAgentModel.PhoneNumber) ? agent.PhoneNumber : updateAgentModel.PhoneNumber;
-            agent.EmailAddress = String.IsNullOrEmpty(updateAgentModel.EmailAddress) ? agent.EmailAddress : updateAgentModel.EmailAddress;
-            agent.Raiting = updateAgentModel.Raiting < 0 ? agent.Raiting : updateAgentModel.Raiting;
-            
-            if(updateAgentModel.CompanyId != null)
-            {
-                agent.CompanyId = (Guid)updateAgentModel.CompanyId;
-                agent.Company = await dbContext.Companies.FindAsync(updateAgentModel.CompanyId);
             }
         }
     }
