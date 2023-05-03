9b207d012ec642b356a6fd530cb2fcbc992e0c73
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-03 16:21:03 +0300

    Create INutritionService.cs

---
+++ InsuranceDiscountsWeb/BLL/Interfaces/INutritionService.cs
@@ -0,0 +1,25 @@
+﻿using Core.Models;
+using Core.Models.UpdateModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface INutritionService
+    {
+        public Task<Nutrition?> Create(Nutrition nutrition);
+
+        public Task<Nutrition?> GetById(Guid id);
+
+        public Task<List<Nutrition>> GetAll();
+
+        public Task<List<Nutrition>> UserNutritions(Guid userId);
+
+        public Task<bool> Delete(Guid id);
+
+        public Task<Nutrition?> Update(UpdateNutritionModel updateNutrition);
+    }
+}
