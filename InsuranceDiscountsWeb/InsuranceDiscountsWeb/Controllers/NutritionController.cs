be7fc47f0e76283223377c4732f545092d49e898
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-03 16:49:26 +0300

    Create controller

---
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/Controllers/NutritionController.cs
@@ -0,0 +1,35 @@
+﻿using BLL.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceDiscountsWeb.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NutritionController : ControllerBase
+    {
+        private readonly INutritionService nutritionService;
+        private readonly ILogger<NutritionController> logger;
+
+        public NutritionController(
+            INutritionService nutritionService,
+            ILogger<NutritionController> logger
+            )
+        {
+            this.nutritionService = nutritionService;
+            this.logger = logger;
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<IActionResult> GetAll()
+        {
+            try{
+
+            }
+            catch(Exception e)
+            {
+                logger.LogError(e.Message);
+            }
+        }
+    }
+}
