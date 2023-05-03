ca1fb985e852fd4f65627fbc058544603e0a66f6
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-03 23:13:36 +0300

    Create IActivityService.cs

---
+++ InsuranceDiscountsWeb/BLL/Interfaces/IActivityService.cs
@@ -0,0 +1,25 @@
+﻿using Core.Models.UpdateModels;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IActivityService
+    {
+        public Task<Activity?> Create(Activity Activity);
+
+        public Task<Activity?> GetById(Guid id);
+
+        public Task<List<Activity>> GetAll();
+
+        public Task<List<Activity>> UserActivitys(Guid userId);
+
+        public Task<bool> Delete(Guid id);
+
+        public Task<Activity?> Update(UpdateActivityModel updateActivity);
+    }
+}
