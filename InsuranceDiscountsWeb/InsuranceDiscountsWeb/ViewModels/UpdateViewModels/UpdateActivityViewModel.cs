510b52a9cb0ee774428b7d33c47808ee4b03a2ed
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-03 23:16:43 +0300

    Create UpdateActivityViewModel.cs

---
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/ViewModels/UpdateViewModels/UpdateActivityViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InsuranceDiscountsWeb.ViewModels.UpdateViewModels
+{
+    public class UpdateActivityViewModel
+    {
+        [Required]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [MinLength(1)]
+        public string? Type { get; set; }
+
+        [Range(1,100000000)]
+        public double Duration { get; set; } = -1;
+
+        [Range(1, 100000000)]
+        public double Distance { get; set; } = -1;
+
+        [Range(1, 100000000)]
+        public double Calories { get; set; } = -1;
+
+        public Guid? UserId { get; set; }
+    }
+}
