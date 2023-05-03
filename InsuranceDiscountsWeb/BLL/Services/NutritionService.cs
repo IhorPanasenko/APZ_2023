3183ff463b52f0d15da13815a235774f6eb58f64
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-03 16:45:28 +0300

    Implement INutritionService

--- InsuranceDiscountsWeb/BLL/Services/NutritionService.cs
+++ InsuranceDiscountsWeb/BLL/Services/NutritionService.cs
@@ -25,76 +25,111 @@ namespace BLL.Services
             this.logger = logger;
         }
 
-        public Task<Nutrition?> Create(Nutrition nutrition)
+        public async Task<Nutrition?> Create(Nutrition nutrition)
         {
             try
             {
-
+                var newNutrition = await nutritionRepository.Create(nutrition);
+                return newNutrition;
             }
             catch (Exception e)
             {
                 logger.LogError(e.Message);
+                return null;
             }
         }
 
-        public Task<bool> Delete(Guid id)
+        public async Task<bool> Delete(Guid id)
         {
             try
             {
-
+                var res = await nutritionRepository.Delete(id);
+                return res;
             }
             catch (Exception e)
             {
                 logger.LogError(e.Message);
+                return false;
             }
         }
 
-        public Task<List<Nutrition>> GetAll()
+        public async Task<List<Nutrition>> GetAll()
         {
+            List<Nutrition> nutritions = new List<Nutrition>();
+
             try
             {
-
+                nutritions = await nutritionRepository.GetAll();
             }
             catch (Exception e)
             {
                 logger.LogError(e.Message);
+            }
+
+            return nutritions;
+        }
+
+        public async Task<Nutrition?> GetById(Guid id)
+        {
+            try
+            {
+                var nutrition = await nutritionRepository.GetById(id);
+                return nutrition;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return null;
             }
         }
 
-        public Task<Nutrition?> GetById(Guid id)
+        public async Task<Nutrition?> Update(UpdateNutritionModel updateNutrition)
         {
             try
             {
+                var oldNutrition = await nutritionRepository.GetById(updateNutrition.Id);
 
+                if(oldNutrition is null)
+                {
+                    throw new ArgumentException($"No nutrition with id {updateNutrition.Id}");
+                }
+
+                update(oldNutrition, updateNutrition);
+                var newNutrition = await nutritionRepository.Update(oldNutrition);
+                return newNutrition;
             }
             catch (Exception e)
             {
                 logger.LogError(e.Message);
+                return null;
             }
         }
 
-        public Task<Nutrition?> Update(UpdateNutritionModel updateNutrition)
+        private void update(Nutrition oldNutrition, UpdateNutritionModel updateNutrition)
         {
-            try
-            {
-
-            }
-            catch (Exception e)
-            {
-                logger.LogError(e.Message);
-            }
+            oldNutrition.Id = updateNutrition.Id;
+            oldNutrition.Meal = String.IsNullOrEmpty(updateNutrition.Meal) ? oldNutrition.Meal : updateNutrition.Meal;
+            oldNutrition.Food = String.IsNullOrEmpty(updateNutrition.Food) ? oldNutrition.Food : updateNutrition.Food;
+            oldNutrition.Calories = updateNutrition.Calories <= 0 ? oldNutrition.Calories : updateNutrition.Calories;
+            oldNutrition.Fat = updateNutrition.Fat <= 0 ? oldNutrition.Fat : updateNutrition.Fat;
+            oldNutrition.Protein = updateNutrition.Protein <=0 ? oldNutrition.Protein : updateNutrition.Protein;
+            oldNutrition.UserId = updateNutrition.UserId == null ? oldNutrition.UserId : (Guid)updateNutrition.UserId;
         }
 
-        public Task<List<Nutrition>> UserNutritions(Guid userId)
+        public async Task<List<Nutrition>> UserNutritions(Guid userId)
         {
+            List<Nutrition> nutritions = new List<Nutrition>();
+
             try
             {
-
+                nutritions  = await nutritionRepository.GetAll();
             }
             catch (Exception e)
             {
                 logger.LogError(e.Message);
             }
+
+            return nutritions;
         }
     }
 }
