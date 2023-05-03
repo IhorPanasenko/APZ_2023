e8c5b9aa0f293c051a412f685e4af98b438bd9bb
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-03 22:59:50 +0300

    Create IActivityRepository.cs

---
+++ InsuranceDiscountsWeb/DAL/Interfaces/IActivityRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IActivityRepository
+    {
+        public Task<Activity?> Create(Activity activity);
+
+        public Task<Activity?> GetById(Guid id);
+
+        public Task<List<Activity>> GetAll();
+
+        public Task<List<Activity>> UserActivities(Guid userId);
+
+        public Task<bool> Delete(Guid id);
+
+        public Task<Activity?> Update(Activity updateActivity);
+    }
+}
