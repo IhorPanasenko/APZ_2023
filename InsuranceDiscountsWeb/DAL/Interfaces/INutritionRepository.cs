289d7df65e56b65b2e0f3571820b772d3c49e27d
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-03 16:16:10 +0300

    Some fixes in interface

--- InsuranceDiscountsWeb/DAL/Interfaces/INutritionRepository.cs
+++ InsuranceDiscountsWeb/DAL/Interfaces/INutritionRepository.cs
@@ -10,16 +10,16 @@ namespace DAL.Interfaces
 {
     public interface INutritionRepository
     {
-        public Task<Nutrition> Create(Nutrition nutrition);
+        public Task<Nutrition?> Create(Nutrition nutrition);
 
-        public Task<Nutrition> GetById(Guid id);
+        public Task<Nutrition?> GetById(Guid id);
 
         public Task<List<Nutrition>> GetAll();
         
         public Task<List<Nutrition>> UserNutritions(Guid userId);
 
-        public Task<bool> Delete();
+        public Task<bool> Delete(Guid id);
 
-        public Task<Nutrition> Update(UpdateNutritionModel updateNutrition);
+        public Task<Nutrition?> Update(Nutrition updateNutrition);
     }
 }
