fec12b2ce101227dba002f8a9b612913e45d7139
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-06 12:25:22 +0300

    Create UpdateStaticMeasurmentsViewModel.cs

---
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/ViewModels/UpdateViewModels/UpdateStaticMeasurmentsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InsuranceDiscountsWeb.ViewModels.UpdateViewModels
+{
+    public class UpdateStaticMeasurmentsViewModel
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        public double Height { get; set; } = -1;
+
+        public double Weight { get; set; } = -1;
+
+        public double Waist { get; set; } = -1;
+
+        public DateTime? MesurmentDate { get; set; }
+    }
+}
