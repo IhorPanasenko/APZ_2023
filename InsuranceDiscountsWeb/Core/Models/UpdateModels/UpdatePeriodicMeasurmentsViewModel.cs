0b5ccf98adad3ff78caf029c8af9a903c17d982d
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-06 13:15:07 +0300

    Create UpdatePeriodicMeasurmentsViewModel.cs

---
+++ InsuranceDiscountsWeb/Core/Models/UpdateModels/UpdatePeriodicMeasurmentsViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Models.UpdateModels
+{
+    public class UpdatePeriodicMeasurmentsViewModel
+    {
+        [Required]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public int Pulse { get; set; } = -1;
+
+        public double Glucose { get; set; } = -1;
+
+        public double Cholesterol { get; set; } = -1;
+
+        public double BloodPreasure { get; set; } = -1;
+        public DateTime? MesurmentDate { get; set; }
+    }
+}
