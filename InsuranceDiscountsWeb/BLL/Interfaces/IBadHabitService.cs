7dc021a51f320838d543038b76efa163a5b6d6da
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-06 18:53:34 +0300

    Create IBadHabitService.cs

---
+++ InsuranceDiscountsWeb/BLL/Interfaces/IBadHabitService.cs
@@ -0,0 +1,22 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IBadHabitService
+    {
+        public Task<List<BadHabit>> GetAll();
+
+        public Task<List<BadHabit>> Get(Guid id);
+
+        public Task<BadHabit?> Create(BadHabit badHabit);
+
+        public Task<bool> Delete(Guid id);
+
+        public Task<BadHabit?> Update(BadHabit badHabit);
+    }
+}
