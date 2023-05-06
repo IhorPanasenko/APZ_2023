56b29a5e48a07fbc1ee3c6a69751202b6de4ad3d
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-06 19:45:26 +0300

    Create UpdateBadHabitViewModel.cs

---
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/ViewModels/UpdateViewModels/UpdateBadHabitViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InsuranceDiscountsWeb.ViewModels.UpdateViewModels
+{
+    public class UpdateBadHabitViewModel
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        [Range(0, 100)]
+        public int Level { get; set; } = -1;
+    }
+}
