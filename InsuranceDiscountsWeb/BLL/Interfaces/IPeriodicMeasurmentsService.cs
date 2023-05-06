4ed0ab1d81756f2b410cf6386529b0faf7f1f0a9
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-06 13:24:13 +0300

    Update IPeriodicMeasurmentsService.cs

--- InsuranceDiscountsWeb/BLL/Interfaces/IPeriodicMeasurmentsService.cs
+++ InsuranceDiscountsWeb/BLL/Interfaces/IPeriodicMeasurmentsService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Core.Models.UpdateModels;
+using Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +10,16 @@ namespace BLL.Interfaces
 {
     public interface IPeriodicMeasurmentsService
     {
+        public Task<PeriodicMeasurments?> Create(PeriodicMeasurments PeriodicMeasurments);
 
+        public Task<PeriodicMeasurments?> GetById(Guid id);
+
+        public Task<List<PeriodicMeasurments>> GetAll();
+
+        public Task<List<PeriodicMeasurments>> UserPeriodicMeasurmentss(Guid userId);
+
+        public Task<bool> Delete(Guid id);
+
+        public Task<PeriodicMeasurments?> Update(UpdatePeriodicMeasurmentsModel updatePeriodicMeasurments);
     }
 }
