e38931c006cadd5f69a0ebf64d6f0ed17ec48461
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-06 12:01:03 +0300

    Create IStaticMeasurmentsRepository.cs

---
+++ InsuranceDiscountsWeb/DAL/Interfaces/IStaticMeasurmentsRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IStaticMeasurmentsRepository
+    {
+        public Task<StaticMeasurments?> Create(StaticMeasurments staticMeasurments);
+
+        public Task<StaticMeasurments?> GetById(Guid id);
+
+        public Task<List<StaticMeasurments>> GetAll();
+
+        public Task<List<StaticMeasurments>> UserStaticMeasurmentss(Guid userId);
+
+        public Task<bool> Delete(Guid id);
+
+        public Task<StaticMeasurments?> Update(StaticMeasurments updateStaticMeasurments);
+    }
+}
