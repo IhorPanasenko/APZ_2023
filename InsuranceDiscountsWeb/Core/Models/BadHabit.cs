107fbfb1642709f1ec079a82ac648a7e37b91ea0
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-06 17:16:12 +0300

    Create BadHabitModel

---
+++ InsuranceDiscountsWeb/Core/Models/BadHabit.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.Models
+{
+    public class BadHabit
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = String.Empty;
+
+        [Required]
+        [Range(0,100)]
+        public int Level { get; set; }
+    }
+}
