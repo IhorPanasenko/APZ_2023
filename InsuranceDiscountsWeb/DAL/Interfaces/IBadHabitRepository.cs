af15f0c52ac2c2b47a0c7d876691b9c8c99a9c9c
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-06 17:23:28 +0300

    Create IBadHabitRepository.cs

---
+++ InsuranceDiscountsWeb/DAL/Interfaces/IBadHabitRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IBadHabitRepository
+    {
+        public Task<List<BadHabit>> GetAll();
+
+        public Task<BadHabit> GetById(Guid id);
+
+        public Task<BadHabit> Create(BadHabit badHabit);
+
+        public Task<bool> Delete(Guid id);
+
+        public Task<BadHabit> Update(BadHabit badHabit);
+    }
+}
