abe5a4e13c9a5f97fd22e03a70e58d21c6cfb1ed
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-07 13:31:03 +0300

    Update UserBadHabitViewModel.cs

--- InsuranceDiscountsWeb/InsuranceDiscountsWeb/ViewModels/UserBadHabitViewModel.cs
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/ViewModels/UserBadHabitViewModel.cs
@@ -7,12 +7,10 @@ namespace InsuranceDiscountsWeb.ViewModels
     {
         public Guid Id { get; set; } = Guid.NewGuid();
 
-        [ForeignKey("UserId")]
         public Guid UserId { get; set; }
 
         public AppUser? AppUser { get; set; }
 
-        [ForeignKey("BadHabitId")]
         public Guid BadHabitId { get; set; }
 
         public BadHabit? BadHabit { get; set; }
