b343278d2ac97bc4f5c870a527cf5e50eeb22b9f
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-07 09:25:27 +0300

    Add Properties To User

--- InsuranceDiscountsWeb/Core/Models/AppUser.cs
+++ InsuranceDiscountsWeb/Core/Models/AppUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,8 +11,14 @@ namespace Core.Models
 {
     public class AppUser :IdentityUser
     {
-        public string FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = String.Empty;
+
+        public string LastName { get; set; } = String.Empty;
+
+        public string Address { get; set; } = String.Empty;
+
+        [Range(0,50)]
+        public double Discount { get; set; } = 0;
 
         [NotMapped]
         public List<string> UserRoles { get; set; } = new List<string>();
