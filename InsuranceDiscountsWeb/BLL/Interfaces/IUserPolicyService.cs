f2bbdfe129fea9fa331c1693fd1bf60d1ba3431a
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-07 13:31:24 +0300

    Create IUserPolicyService.cs

---
+++ InsuranceDiscountsWeb/BLL/Interfaces/IUserPolicyService.cs
@@ -0,0 +1,20 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Interfaces
+{
+    public interface IUserPolicyService
+    {
+        public Task<UserPolicies?> Create(UserPolicies userUserPolicies);
+
+        public Task<bool> Delete(Guid id);
+
+        public Task<List<UserPolicies>> GetAll();
+
+        public Task<List<UserPolicies>> GetByUser(Guid userId);
+    }
+}
