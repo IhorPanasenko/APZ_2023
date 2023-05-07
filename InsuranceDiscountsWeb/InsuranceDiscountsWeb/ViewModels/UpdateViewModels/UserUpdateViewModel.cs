a073b566474923bf596c6569b7beca05deb7b2c6
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-07 09:34:42 +0300

    Move File to Update View Model Folder

---
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/ViewModels/UpdateViewModels/UserUpdateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InsuranceDiscountsWeb.ViewModels.UpdateViewModels
+{
+    public class UserUpdateViewModel
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? UserName { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Phone]
+        public string? PhoneNumber { get; set; }
+
+        public string? Address { get; set; }
+
+    }
+}
