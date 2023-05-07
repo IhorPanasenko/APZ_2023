89b24ad400c2e8dcf4ed93b2c7d36d63e39c2177
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-07 09:25:42 +0300

    Create Migration and update database

---
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/Migrations/20230507062432_AddFieldsToUser.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsuranceDiscountsWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddFieldsToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Discount",
+                table: "AspNetUsers",
+                type: "float",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Discount",
+                table: "AspNetUsers");
+        }
+    }
+}
