953d318f48740ec8c58fc1911f9f6fc09b1b9b5e
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-08 18:06:01 +0300

    Add migration and update database

---
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/Migrations/20230508150413_addColumn.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsuranceDiscountsWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class addColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserPolicies_AspNetUsers_UserId1",
+                table: "UserPolicies");
+
+            migrationBuilder.RenameColumn(
+                name: "UserId1",
+                table: "UserPolicies",
+                newName: "AppUserId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserPolicies_UserId1",
+                table: "UserPolicies",
+                newName: "IX_UserPolicies_AppUserId");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MaxDiscountPercentage",
+                table: "Companies",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthdayDate",
+                table: "AspNetUsers",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserPolicies_AspNetUsers_AppUserId",
+                table: "UserPolicies",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserPolicies_AspNetUsers_AppUserId",
+                table: "UserPolicies");
+
+            migrationBuilder.DropColumn(
+                name: "MaxDiscountPercentage",
+                table: "Companies");
+
+            migrationBuilder.DropColumn(
+                name: "BirthdayDate",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "AppUserId",
+                table: "UserPolicies",
+                newName: "UserId1");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_UserPolicies_AppUserId",
+                table: "UserPolicies",
+                newName: "IX_UserPolicies_UserId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserPolicies_AspNetUsers_UserId1",
+                table: "UserPolicies",
+                column: "UserId1",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
