e2ce4743c18a6fae704c60364661a2ce644ea65d
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-08 17:43:28 +0300

    Delete unnecessary usings

--- InsuranceDiscountsWeb/BLL/Services/UserService.cs
+++ InsuranceDiscountsWeb/BLL/Services/UserService.cs
@@ -2,22 +2,10 @@
 using Core.Models;
 using Core.Models.UpdateModels;
 using DAL.Interfaces;
-using DAL.Services;
-using InsuranceDiscountsWeb.Managers;
-using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
-using System;
-using System.Collections.Generic;
-using System.IdentityModel.Tokens.Jwt;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace BLL.Services
 {
