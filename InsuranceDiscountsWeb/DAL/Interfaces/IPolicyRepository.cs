b9868a21bd4c0124e91ad1961f5e5956f676ccc3
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-13 00:42:30 +0300

    Create IPolicyRepository

---
+++ InsuranceDiscountsWeb/DAL/Interfaces/IPolicyRepository.cs
@@ -0,0 +1,22 @@
+﻿using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface IPolicyRepository
+    {
+        public Task<bool> Create(Policy policy);
+
+        public Task<bool> Update(Policy policy);
+
+        public Task<List<Policy>> GetAll();
+
+        public Task<bool> Delete(Guid id);
+
+        public Task<Policy?> GetById(Guid id);
+    }
+}
