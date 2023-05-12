f8d781f8deb7e421b87a3e5b8a447cbad5c94361
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-13 00:42:40 +0300

    CreatepolicyRepository

---
+++ InsuranceDiscountsWeb/DAL/Repositories/PolicyRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repositories
+{
+    public class PolicyRepository
+    {
+        private readonly ILogger<AgentRepository> logger;
+        private readonly InsuranceDiscountsDbContext dbContext;
+
+        public PolicyRepository(ILogger<AgentRepository> logger, InsuranceDiscountsDbContext dbContext)
+        {
+            this.logger = logger;
+            this.dbContext = dbContext;
+        }
+
+
+    }
+}
