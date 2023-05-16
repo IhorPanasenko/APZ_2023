9b9cb13045006b3f172d7d2026729998c3d939bf
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-16 08:11:57 +0300

    Create UpdatePolicyViewModel.cs

---
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/ViewModels/UpdateViewModels/UpdatePolicyViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace InsuranceDiscountsWeb.ViewModels.UpdateViewModels
+{
+    public class UpdatePolicyViewModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public double CoverageAmount { get; set; } = -1;
+
+        public double Price { get; set; } = -1;
+
+        public int TimePeriod { get; set; } = -1;
+
+        public Guid? CompanyId { get; set; }
+
+        public Guid? CategoryId { get; set; }
+    }
+}
