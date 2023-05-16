a6b3c0136cd6c9b9bf30ff5e6480130763616fe0
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-16 08:11:49 +0300

    Create PolicyViewModel.cs

---
+++ InsuranceDiscountsWeb/InsuranceDiscountsWeb/ViewModels/PolicyViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Core.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace InsuranceDiscountsWeb.ViewModels
+{
+    public class PolicyViewModel
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string Name { get; set; } = String.Empty;
+
+        [Required]
+        public string Description { get; set; } = String.Empty;
+
+        [Required]
+        public double CoverageAmount { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public int TimePeriod { get; set; } = 12;
+
+        public Guid CompanyId { get; set; }
+
+        public Company? Company { get; set; }
+
+        public Guid CategoryId { get; set; }
+
+        public Category? Category { get; set; }
+    }
+}
