28c2760aab7cd90ba8979fcdfcb5d078a52e0fa2
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-16 08:11:27 +0300

    Update PolicyRepository.cs

--- InsuranceDiscountsWeb/DAL/Repositories/PolicyRepository.cs
+++ InsuranceDiscountsWeb/DAL/Repositories/PolicyRepository.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Core.Models;
+using DAL.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DAL.Repositories
 {
-    public class PolicyRepository
+    public class PolicyRepository : IPolicyRepository
     {
         private readonly ILogger<AgentRepository> logger;
         private readonly InsuranceDiscountsDbContext dbContext;
@@ -18,6 +22,127 @@ namespace DAL.Repositories
             this.dbContext = dbContext;
         }
 
+        public async Task<Policy?> Create(Policy policy)
+        {
+            try
+            {
+                await dbContext.Policies.AddAsync(policy);
+                await dbContext.SaveChangesAsync();
 
+                var company = await dbContext.Companies.FindAsync(policy.CompanyId);
+                policy.Company = company;
+                policy.Category = await dbContext.Categories.FindAsync(policy.CategoryId);
+                return policy;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return null;
+            }
+        }
+
+        public async Task<bool> Delete(Guid id)
+        {
+            try
+            {
+                var policy = await dbContext.Policies.FindAsync(id);
+
+                if (policy is null)
+                {
+                    throw new Exception($"No policy with id {id} was found");
+                }
+
+                dbContext.Policies.Remove(policy);
+                await dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return false;
+            }
+        }
+
+        public async Task<List<Policy>> GetAll()
+        {
+            List<Policy> policies = new List<Policy>();
+
+            try
+            {
+                policies = await dbContext.Policies.ToListAsync();
+
+                foreach (var policy in policies)
+                {
+                    var company = await dbContext.Companies.FindAsync(policy.CompanyId);
+
+                    if (company is not null)
+                    {
+                        policy.Company = company;
+                    }
+
+                    var category = await dbContext.Categories.FindAsync(policy.CategoryId);
+
+                    if (category is not null)
+                    {
+                        policy.Category = category;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+            }
+
+            return policies;
+        }
+
+        public async Task<Policy?> GetById(Guid id)
+        {
+            try
+            {
+                var policy = await dbContext.Policies.FindAsync(id);
+
+                if (policy is null)
+                {
+                    throw new Exception($"Can't get policy with id {id}");
+                }
+
+                var company = await dbContext.Companies.FindAsync(policy.CompanyId);
+
+                if (company is not null)
+                {
+                    policy.Company = company;
+                }
+
+                var category = await dbContext.Categories.FindAsync(policy.CategoryId);
+
+                if (category is not null)
+                {
+                    policy.Category = category;
+                }
+
+                return policy;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return null;
+            }
+        }
+
+        public async Task<Policy?> Update(Policy policy)
+        {
+            try
+            {
+                dbContext.Policies.Update(policy);
+                await dbContext.SaveChangesAsync();
+                return policy;
+            }
+            catch (Exception e)
+            {
+                logger.LogError(e.Message);
+                return null;
+            }
+        }
     }
 }
