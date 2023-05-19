33e4087f8816c58168452e46c7599db200f1252e
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-20 00:27:09 +0300

    Update index.js

--- front-project/src/index.js
+++ front-project/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './localization/config';
 import { I18nextProvider } from 'react-i18next';
+import i18n from './localization/config';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <I18nextProvider>
+  <I18nextProvider i18n={i18n}>
     <React.StrictMode>
       <App />
     </React.StrictMode>
