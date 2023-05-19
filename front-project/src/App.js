c576fbeca791bd773778d9ec125bbdcd3527c75b
IhorPanasenko <ihor.panasenko1@nure.ua>
2023-05-20 00:28:10 +0300

    Update App.js

--- front-project/src/App.js
+++ front-project/src/App.js
@@ -1,30 +1,16 @@
 import React from "react";
-import { useTranslation} from "react-i18next";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login/Login";
+import RegisterPage from "./pages/reister/RegisterPage";
 
 function App() {
-  const { t } = useTranslation();
-  const { i18n }  = useTranslation();
-  let lang = "uk";
-
-  function clickhandler() {
-    console.log(lang);
-    console.log(lang === "uk");
-    if (lang === "uk") {
-      i18n.changeLanguage("en");
-      lang = "en";
-    }
-    else {
-      i18n.changeLanguage("uk");
-      lang = "uk";
-    }
-  }
-
   return (
-    <>
-      <h1>Hello World</h1>
-      <div>{t("hello")}</div>
-      <button onClick={clickhandler}>Change</button>
-    </>
+      <Router>
+        <Routes>
+          <Route path="/" Component={Login} />
+          <Route path="/registration" Component={RegisterPage}/>
+        </Routes>
+      </Router>
   );
 }
 
